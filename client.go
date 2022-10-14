8433a8bd06b5b6b7dca9e37bd40d1a91041b3eac
yingjian <yingjian@bianjie.ai>
2022-10-14 17:59:21 +0800

    modify node code

--- client.go
+++ client.go
@@ -4,6 +4,7 @@ import (
 	common "github.com/kaifei-bianjie/common-parser"
 	"github.com/kaifei-bianjie/iritamod-parser/codec"
 	"github.com/kaifei-bianjie/iritamod-parser/modules/identity"
+	"github.com/kaifei-bianjie/iritamod-parser/modules/node"
 	"github.com/kaifei-bianjie/iritamod-parser/modules/params"
 	"github.com/kaifei-bianjie/iritamod-parser/modules/perm"
 	"github.com/kaifei-bianjie/iritamod-parser/modules/slashing"
@@ -16,6 +17,7 @@ type MsgClient struct {
 	Upgrade  common.Client
 	Identity common.Client
 	Perm     common.Client
+	Node     common.Client
 }
 
 func NewMsgClient() MsgClient {
@@ -26,5 +28,6 @@ func NewMsgClient() MsgClient {
 		Upgrade:  upgrade.NewClient(),
 		Identity: identity.NewClient(),
 		Perm:     perm.NewClient(),
+		Node:     node.NewClient(),
 	}
 }
